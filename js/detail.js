f57447572f021bb3df8382bc0bbf97db367030d4
huang <123213.@qq>
2019-07-13 19:03:37 +0800

    modify7.13

--- js/detail.js
+++ js/detail.js
@@ -19,11 +19,11 @@ $(function () {
     let addBtn = $('.add');
     let reduceBtn = $('.reduce');
     // 点击＋，数量加一
-    addBtn.on('click',function(){
+    addBtn.on('click', function () {
         // 获取原来是多少件
         let old = parseInt(chooseNumber.val());
         old++;
-        if(old >1){
+        if (old > 1) {
             reduceBtn.removeClass('disabled')
         }
         // 把新的值赋给输入框
@@ -31,49 +31,49 @@ $(function () {
     })
 
     // 点击－号，数量减一，但数量最少不能少于1
-    reduceBtn.on('click',function(){
-                // 获取原来是多少件
-                let old = parseInt(chooseNumber.val());
-                if(old ===1){
-                    return
-                }
-                old--;
-                if(old === 1){
-                    reduceBtn.addClass('disabled')
-                }
-                  // 把新的值赋给输入框
+    reduceBtn.on('click', function () {
+        // 获取原来是多少件
+        let old = parseInt(chooseNumber.val());
+        if (old === 1) {
+            return
+        }
+        old--;
+        if (old === 1) {
+            reduceBtn.addClass('disabled')
+        }
+        // 把新的值赋给输入框
         chooseNumber.val(old);
     })
 
 
 
     // 点击加入购物车
-    $('.addshopcar').on('click',function(){
+    $('.addshopcar').on('click', function () {
         // 把当前对应的商品加入购物车
-        let number =parseInt( $('.choose-number').val());/*得到商品数量*/
+        let number = parseInt($('.choose-number').val()); /*得到商品数量*/
         // 把每个数据存到localStorage里，因为有多个商品所以用数组方式存储
         // let arr = [];
         // 先从localStorage里读取旧的数据，然后新旧数据叠加
         let jsonStr = localStorage.getItem('shopCarData');
         let arr;
-        if(jsonStr ===null){
-            arr =[];
-        }else{
+        if (jsonStr === null) {
+            arr = [];
+        } else {
             arr = JSON.parse(jsonStr);
         }
 
         let good = {
-            pID:obj.pID,
-            name:obj.name,
-            imgSrc:obj.imgSrc,
-            price:obj.price,
-            number:number
+            pID: obj.pID,
+            name: obj.name,
+            imgSrc: obj.imgSrc,
+            price: obj.price,
+            number: number
         }
         arr.push(good);
-        
+
         // 把数组转换成JSON格式字符串存放到localStorage里
         jsonStr = JSON.stringify(arr);
-        localStorage.setItem('shopCartData',jsonStr)
+        localStorage.setItem('shopCartData', jsonStr)
 
     })
 })
